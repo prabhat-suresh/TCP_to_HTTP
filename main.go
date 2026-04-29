425cae6ba54f320ca8f66d84be31728b6b843226
Prabhat Suresh <prabhatsuresh5@gmail.com>
2026-04-29 06:39:13 +0530

    Ch1 L3: 8 bytes

--- main.go
+++ main.go
@@ -1,5 +1,30 @@
 package main
 
+import (
+	"errors"
+	"fmt"
+	"io"
+	"log"
+	"os"
+)
+
 func main() {
-	println("I hope I get the job!")
+	file, err := os.Open("messages.txt")
+	if err != nil {
+		log.Fatal("Error: ", err)
+	}
+	buffer := make([]byte, 8)
+	for {
+		numBytesRead, err := file.Read(buffer)
+		if err != nil {
+			if errors.Is(err, io.EOF) {
+				// EOF
+				break
+			} else {
+				// unexpected extraneous error
+				log.Fatal("Error: ", err)
+			}
+		}
+		fmt.Printf("read: %s\n", buffer[:numBytesRead])
+	}
 }
