d516d1dc2e8461aa25df1b4d577a7f3a8ef5da12
Prabhat Suresh <prabhatsuresh5@gmail.com>
2026-04-29 08:41:09 +0530

    Ch1 L5: Channel Refactor

--- main.go
+++ main.go
@@ -9,34 +9,47 @@ import (
 	"os"
 )
 
+func getLinesChannel(f io.ReadCloser) <-chan string {
+	ch := make(chan string)
+
+	go func() {
+		defer close(ch)
+		buffer := make([]byte, 8)
+		contents := make([]byte, 0)
+		for {
+			numBytesRead, err := f.Read(buffer)
+			if err != nil {
+				if errors.Is(err, io.EOF) {
+					// EOF
+					break
+				} else {
+					// unexpected extraneous error
+					log.Fatal("Error: ", err)
+				}
+			}
+			if newlineIndex := bytes.IndexByte(buffer[:numBytesRead], '\n'); newlineIndex != -1 {
+				contents = append(contents, buffer[:newlineIndex]...)
+				ch <- string(contents)
+				contents = contents[:0]
+				contents = append(contents, buffer[newlineIndex+1:numBytesRead]...)
+			} else {
+				contents = append(contents, buffer[:numBytesRead]...)
+			}
+		}
+		if len(contents) > 0 {
+			ch <- string(contents)
+		}
+	}()
+	return ch
+}
+
 func main() {
 	file, err := os.Open("messages.txt")
 	if err != nil {
 		log.Fatal("Error: ", err)
 	}
-	buffer := make([]byte, 8)
-	contents := make([]byte, 0)
-	for {
-		numBytesRead, err := file.Read(buffer)
-		if err != nil {
-			if errors.Is(err, io.EOF) {
-				// EOF
-				break
-			} else {
-				// unexpected extraneous error
-				log.Fatal("Error: ", err)
-			}
-		}
-		if newlineIndex := bytes.IndexByte(buffer[:numBytesRead], '\n'); newlineIndex != -1 {
-			contents = append(contents, buffer[:newlineIndex]...)
-			fmt.Printf("read: %s\n", contents)
-			contents = contents[:0]
-			contents = append(contents, buffer[newlineIndex+1:numBytesRead]...)
-		} else {
-			contents = append(contents, buffer[:numBytesRead]...)
-		}
-	}
-	if len(contents) > 0 {
-		fmt.Printf("read: %s\n", contents)
+	defer file.Close()
+	for line := range getLinesChannel(file) {
+		fmt.Printf("read: %s\n", line)
 	}
 }
