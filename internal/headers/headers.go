5e0b633d81b60d8dba2ae512591d0a1d5f640ce5
Prabhat Suresh <prabhatsuresh5@gmail.com>
2026-05-02 18:14:49 +0530

    Ch5 L2: Constraints

--- internal/headers/headers.go
+++ internal/headers/headers.go
@@ -3,6 +3,7 @@ package headers
 import (
 	"bytes"
 	"errors"
+	"strings"
 )
 
 type Headers map[string]string
@@ -10,8 +11,32 @@ type Headers map[string]string
 const httpSepCRLF = "\r\n"
 const whitespaceChars = " \n"
 
+func isValidKey(key []byte) bool {
+	if len(key) == 0 {
+		return false
+	}
+	for _, ch := range key {
+		if 'a' < ch && ch < 'z' || 'A' < ch && ch < 'Z' || '0' < ch && ch < '9' {
+			continue
+		}
+		switch ch {
+		case '!', '#', '$', '%', '&', '\'', '*', '+', '-', '.', '^', '_', '`', '|', '~':
+		default:
+			return false
+		}
+	}
+	return true
+}
+
 func NewHeaders() Headers {
 	return make(Headers)
+}
+
+func (h Headers) Get(key string) string {
+	return h[strings.ToLower(key)]
+}
+func (h Headers) Set(key, value string) {
+	h[strings.ToLower(key)] = value
 }
 
 func (h Headers) Parse(data []byte) (n int, done bool, err error) {
@@ -30,15 +55,12 @@ func (h Headers) Parse(data []byte) (n int, done bool, err error) {
 		return 0, false, errors.New("Malformed Header")
 	}
 	key, value := headerParts[0], bytes.TrimSpace(headerParts[1])
-	if len(key) == 0 {
-		return 0, false, errors.New("Key not found")
-	}
-	if bytes.ContainsAny(key, whitespaceChars) {
-		return 0, false, errors.New("Whitespace found around key")
+	if !isValidKey(key) {
+		return 0, false, errors.New("Invalid Key")
 	}
 	if bytes.ContainsAny(value, whitespaceChars) {
 		return 0, false, errors.New("Whitespace found inside value")
 	}
-	h[string(key)] = string(value)
+	h.Set(string(key), string(value))
 	return idx + len(httpSepCRLF), false, nil
 }
--- internal/headers/headers_test.go
+++ internal/headers/headers_test.go
@@ -14,13 +14,21 @@ func TestRequestHeadersParse(t *testing.T) {
 	n, done, err := headers.Parse(data)
 	require.NoError(t, err)
 	require.NotNil(t, headers)
-	assert.Equal(t, "localhost:42069", headers["Host"])
+	assert.Equal(t, "localhost:42069", headers.Get("Host"))
 	assert.Equal(t, 23, n)
 	assert.False(t, done)
 
 	// Test: Invalid spacing header
 	headers = NewHeaders()
 	data = []byte("       Host: localhost:42069\r\n\r\n")
+	n, done, err = headers.Parse(data)
+	require.Error(t, err)
+	assert.Equal(t, 0, n)
+	assert.False(t, done)
+
+	// Test: Invalid character in key
+	headers = NewHeaders()
+	data = []byte("H©st: localhost:42069\r\n\r\n")
 	n, done, err = headers.Parse(data)
 	require.Error(t, err)
 	assert.Equal(t, 0, n)
