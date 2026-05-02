5d04919e306e0f5570d5561a34a2cd13d5fe233a
Prabhat Suresh <prabhatsuresh5@gmail.com>
2026-05-02 14:15:42 +0530

    Ch4 L3: Parsing a Stream

--- internal/request/request.go
+++ internal/request/request.go
@@ -1,10 +1,9 @@
 package request
 
 import (
+	"bytes"
 	"errors"
 	"io"
-	"log"
-	"strings"
 )
 
 type Request struct {
@@ -17,36 +16,73 @@ type RequestLine struct {
 	Method        string
 }
 
-const sep = "\r\n"
+type parseState int
+
+const (
+	initialized parseState = iota
+	done
+)
+
+type parserState struct {
+	state           parseState
+	bytesToBeParsed []byte
+}
+
+const httpSepCRLF = "\r\n"
+const initBufferSize = 8
 
 func RequestFromReader(reader io.Reader) (*Request, error) {
-	var req Request
-	plainReq, err := io.ReadAll(reader)
-	if err != nil {
-		log.Fatal(err)
+	var req *Request
+	parserState := parserState{
+		state:           initialized,
+		bytesToBeParsed: make([]byte, 0, initBufferSize),
 	}
-	plainReqLines := strings.Split(string(plainReq), sep)
-	reqLine, _, err := parseRequestLine(plainReqLines)
+	buf := make([]byte, initBufferSize)
+	for parserState.state != done {
+		numBytesRead, err := reader.Read(buf)
+		if err != nil {
+			return nil, err
+		}
+		req, err = parserState.parse(buf[:numBytesRead])
+		if err != nil {
+			return nil, err
+		}
+	}
+	return req, nil
+}
+
+func (p *parserState) parse(data []byte) (*Request, error) {
+	var req Request
+	p.bytesToBeParsed = append(p.bytesToBeParsed, data...)
+	reqLine, numBytesParsed, err := parseRequestLine(p.bytesToBeParsed)
 	if err != nil {
 		return nil, err
 	}
+	if numBytesParsed == 0 {
+		return nil, nil
+	}
 	req.RequestLine = *reqLine
+	p.state = done
 	return &req, nil
 }
 
-func parseRequestLine(plainReqLines []string) (*RequestLine, []string, error) {
-	reqLine := strings.Split(plainReqLines[0], " ")
+func parseRequestLine(plainReqLine []byte) (*RequestLine, int, error) {
+	idx := bytes.Index(plainReqLine, []byte(httpSepCRLF))
+	if idx == -1 {
+		return nil, 0, nil
+	}
+	reqLine := bytes.Split(plainReqLine[:idx], []byte(" "))
 	if len(reqLine) != 3 {
-		return nil, plainReqLines, errors.New("Invalid number of parts in request line")
+		return nil, 0, errors.New("Invalid number of parts in request line")
 	}
 	method, target, version := reqLine[0], reqLine[1], reqLine[2]
 	for _, c := range method {
 		if c < 'A' || c > 'Z' {
-			return nil, plainReqLines, errors.New("Method should consist of only Capital Letters")
+			return nil, 0, errors.New("Method should consist of only Capital Letters")
 		}
 	}
-	if version != "HTTP/1.1" {
-		return nil, plainReqLines, errors.New("Unsupported HTTP version")
+	if string(version) != "HTTP/1.1" {
+		return nil, 0, errors.New("Unsupported HTTP version")
 	}
-	return &RequestLine{"1.1", target, method}, plainReqLines[1:], nil
+	return &RequestLine{"1.1", string(target), string(method)}, idx + len(httpSepCRLF), nil
 }
